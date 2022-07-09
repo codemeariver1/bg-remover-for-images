a237227011f01e4098e0698927a95dd1d596929b
codemeariver1 <91358795+gucciflipflops1@users.noreply.github.com>
2022-07-08 23:46:16 -0400

    Added updates

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import mediapipe as mp
 # Parent Directory path
 parent_dir = "/Users/bigdaddy/Desktop/Code projects/bg-remover-for-images/"
 
+print("######################################################################")
+
+print("Running checks:") # Create folders if needed
 # Create output folder if doesn't exist
 try:
     # Target directory name
@@ -16,9 +19,9 @@ try:
     
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
-    print("Directory '% s' created" % directory)
+    print("\tDirectory '% s' created" % directory)
 except:
-    print('segmented_images folder already exists, continuing process')
+    print('\tsegmented_images folder already exists, continuing process')
     pass
 
 # Create used images folder if doesn't exist
@@ -28,9 +31,9 @@ try:
     
     used_path = os.path.join(parent_dir, directory)
     os.mkdir(used_path)
-    print("Directory '% s' created" % directory)
+    print("\tDirectory '% s' created\n" % directory)
 except:
-    print('used_images folder already exists, continuing process')
+    print('\tused_images folder already exists, continuing process\n')
     pass
 
 # Read in images folder as a list
@@ -41,17 +44,17 @@ img_list = []
 for img_path in list_img:
     img = cv2.imread(f'images/{img_path}')
     img_list.append(img)
-print(len(img_list))
+print("Image count: ", len(img_list), "\n")
 
 # Create SelfiSegmentation object
 segmentor = SelfiSegmentation()
 
 img_index = 0
 for image in img_list:
-    print("Attempting image ", img_index)
+    print("-> Attempting image", img_index, ":")
     try:
         # Resize the image
-        img = cv2.resize(image, (640, 640))
+        img = cv2.resize(image, (640, 640))     
 
         # Run the segmentor
         img_out = segmentor.removeBG(img, (255, 255, 255), threshold=0.62)
@@ -72,14 +75,16 @@ for image in img_list:
             # Remove original image from images folder
             orig_img_path = os.path.join(parent_dir, "images", list_img[img_index])
             os.remove(orig_img_path)
-        if cv2.waitKey(0) == ord('n'):
-            print('Segmentation of (', list_img[img_index], ') failed continuing...')
-            break
-        print('Segmentation of (', list_img[img_index], ') successful! continuing...')
+        elif cv2.waitKey(0) == ord('n'):
+            print('\tSegmentation of (', list_img[img_index], ') declined continuing...\n')
+            img_index += 1
+            continue
+        print('\tSegmentation of (', list_img[img_index], ') successful! continuing...\n')
+        img_index += 1
     except:
-        print('Segmentation of (', list_img[img_index], ') failed, continuing...')
+        print('\tSegmentation of (', list_img[img_index], ') failed, continuing...\n')
+        img_index += 1
         pass 
 
-    img_index += 1
-
 print("Image segmentation process completed!")
+print("######################################################################")
